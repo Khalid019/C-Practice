6aba9aedec5d89e9a69af67657361e56db6ecc68
Khalid Hossen <52601910+Khalid019@users.noreply.github.com>
2022-12-21 16:28:11 +0600

    max, min, abs, sqrt, round & cmath library

---
+++ max-min-pow-abs.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    double x = 3.11, y = 4.11, z;
+    //z= max(x, y);
+    //z= min(x, y);
+    //z= abs(-3);
+    z= sqrt(x);
+    //z= round(x);
+
+    cout << "The result is: "<< z;
+    return 0;
+}
